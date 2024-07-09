aafd357b364aebc6fefd3f278921c00f68521592
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-09 17:23:44 +0900

    feat: boards

--- public/js/boardFetch.js
+++ public/js/boardFetch.js
@@ -5,7 +5,7 @@ import { fetchData, formatNumber, formatDate } from './fetchData.js';
 import { getBackendDomain } from './config.js';
 
 function processBoardData(data) {
-    const boardData = data.boards;
+    const boardData = data;
     const listBox = document.getElementById('list');
     listBox.innerHTML = '';
 
@@ -15,19 +15,19 @@ function processBoardData(data) {
 
         const postElement = document.createElement('div');
         postElement.classList.add('unit');
-        postElement.setAttribute('onclick', `window.location.href='/boards/detail/${board.board_id}';`);
+        postElement.setAttribute('onclick', `window.location.href='/boards/detail/${board.boardId}';`);
         postElement.innerHTML = `
             <article class="content">
                 <h2 class="title">${board.title}</h2>
                 <article class="detail">
                     <h4 class="board-detail">댓글 ${0}  조회수 ${formatNumber(board.hit)} </h4>
-                    <h4 class="time-detail">${formatDate(board.created_at)} </h4>
+                    <h4 class="time-detail">${formatDate(board.createdAt)} </h4>
                 </article>
             </article>
             <hr class="horizontal-rule"/>   
             <article class="writer">
                 <div class="box">
-                <img class="logo" src="${getBackendDomain()+board.profile_image} " alt="profile-img">
+                <img class="logo" src="${board.profileImage} " alt="profile-img">
                 </div>
                 <h3>${board.nickname}</h3>
             </article>
@@ -44,7 +44,6 @@ Promise.all([
     fetchData('/boards'),
 ]).then(([res]) => {
     console.log(res);
-    console.log(res.data);
-    processBoardData(res.data);
+    processBoardData(res);
 });
 
