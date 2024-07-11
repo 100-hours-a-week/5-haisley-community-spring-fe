fe82da2bce8b0445c840cc0e43104abb77aabdd3
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-11 14:39:46 +0900

    feat: change port

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const app = express();
 // 정적 파일 제공을 위한 미들웨어 설정
 app.use(express.static('public'));
 // 웹 서버가 사용할 포트 번호를 정의합니다.
-const port = 3000;
+const port = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 
