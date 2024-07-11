93368ead93cd826d790082fc5e712f005a3c1c8f
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-11 13:45:44 +0900

    feat: change domain

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
-// const backendDomain = 'http://ec2-43-201-110-205.ap-northeast-2.compute.amazonaws.com';
-const backendDomain = 'http://localhost:8090';
+const backendDomain = 'http://ec2-43-201-110-205.ap-northeast-2.compute.amazonaws.com';
+// const backendDomain = 'http://localhost:8090';
 
 
 function getBackendDomain(){
